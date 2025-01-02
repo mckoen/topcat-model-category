a041277a900fbe94fe877ae5da469570f1e128e3
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-01-02 09:13:12 +0100

    wip

--- TopcatModelCategory/SSet/Boundary.lean
+++ TopcatModelCategory/SSet/Boundary.lean
@@ -11,12 +11,17 @@ namespace standardSimplex
 
 variable {n : ℕ}
 
+@[ext]
+lemma ext {j : SimplexCategoryᵒᵖ} {x y : (Δ[n] : SSet.{u}).obj j}
+    (h : objEquiv _ _ x = objEquiv _ _ y) : x = y :=
+  (objEquiv _ _).injective h
+
 @[simps]
 def face (S : Set (Fin (n + 1))) : (Δ[n] : SSet.{u}).Subobject where
-  obj U := setOf (fun ⟨f⟩ ↦ Set.range f.toOrderHom ⊆ S)
+  obj U := setOf (fun f ↦ Set.range ((objEquiv _ _) f).toOrderHom ⊆ S)
   map := by
     rintro _ _ _ _ hx _ ⟨j, rfl⟩
-    exact hx (by simp)
+    exact hx (by aesop)
 
 lemma face_inter_face (S₁ S₂ : Set (Fin (n + 1))) :
     face S₁ ⊓ face S₂ = face (S₁ ⊓ S₂) := by
@@ -28,9 +33,19 @@ def faceRepresentableBy (S : Set (Fin (n + 1)))
   homEquiv {j} :=
     { toFun f := ⟨objMk ((OrderHom.Subtype.val S).comp
           (e.toOrderEmbedding.toOrderHom.comp f.toOrderHom)), fun _ ↦ by aesop⟩
-      invFun := sorry
-      left_inv := sorry
-      right_inv := sorry }
+      invFun := fun ⟨x, hx⟩ ↦ SimplexCategory.Hom.mk
+        { toFun i := e.symm ⟨(objEquiv _ _ x).toOrderHom i, hx (Set.mem_range_self i)⟩
+          monotone' i₁ i₂ h := e.symm.monotone (by
+            simp only [Subtype.mk_le_mk]
+            exact OrderHom.monotone _ h) }
+      left_inv f := by
+        ext i : 3
+        apply e.symm_apply_apply
+      right_inv := fun ⟨x, hx⟩ ↦ by
+        dsimp
+        ext i : 5
+        exact congr_arg Subtype.val
+          (e.apply_symm_apply ⟨(objEquiv _ _ x).toOrderHom i, _⟩) }
   homEquiv_comp f g := by aesop
 
 end standardSimplex
