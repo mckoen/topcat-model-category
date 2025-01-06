d2fca4c987294ffbf2926423ed9dc86ebf96b59d
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-01-06 23:14:32 +0100

    wip

--- TopCatModelCategory/SSet/AnodyneExtensions.lean
+++ TopCatModelCategory/SSet/AnodyneExtensions.lean
@@ -233,4 +233,16 @@ lemma subcomplex_unionProd_face_ι_mem {X : SSet.{u}} (Y : X.Subcomplex) (i : Fi
       (subcomplexBoundary n)).ι := by
   sorry -- use `subcomplex_unionProd_face_ι_mem` and the skeleton filtration
 
+lemma subcomplex_unionProd_mem_of_left {X Y : SSet.{u}}
+    (A : X.Subcomplex) (B : Y.Subcomplex) (hA : anodyneExtensions A.ι):
+    anodyneExtensions (A.unionProd B).ι := by
+  sorry
+
+lemma subcomplex_unionProd_mem_of_right {X Y : SSet.{u}}
+    (A : X.Subcomplex) (B : Y.Subcomplex) (hB : anodyneExtensions B.ι):
+    anodyneExtensions (A.unionProd B).ι := by
+  refine (anodyneExtensions.arrow_mk_iso_iff ?_).2
+    (subcomplex_unionProd_mem_of_left B A hB)
+  sorry
+
 end anodyneExtensions
