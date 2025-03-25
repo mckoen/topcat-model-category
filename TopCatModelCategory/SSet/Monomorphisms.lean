187042b1686f83794b5a9631a5c42a78334587a0
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-03-25 17:35:07 +0100

    wip

--- TopCatModelCategory/SSet/Monomorphisms.lean
+++ TopCatModelCategory/SSet/Monomorphisms.lean
@@ -1,30 +1,44 @@
 import TopCatModelCategory.SSet.CategoryWithFibrations
 import TopCatModelCategory.ULift
 import Mathlib.CategoryTheory.MorphismProperty.TransfiniteComposition
+import Mathlib.CategoryTheory.MorphismProperty.FunctorCategory
+import Mathlib.CategoryTheory.Types.Monomorphisms
 import Mathlib.CategoryTheory.SmallObject.TransfiniteCompositionLifting
 
 universe w v u
 
-open CategoryTheory MorphismProperty
+open CategoryTheory MorphismProperty Limits
 
 namespace CategoryTheory.MorphismProperty
 
-variable {C : Type u} [Category.{v} C] (W : MorphismProperty C)
+variable {C : Type u} [Category.{v} C]
 
 @[simp]
-lemma rlp_transfiniteCompositions :
+lemma rlp_transfiniteCompositions (W : MorphismProperty C) :
     (transfiniteCompositions.{w} W).rlp = W.rlp := by
   apply le_antisymm
   · exact antitone_rlp W.le_transfiniteCompositions
   · rw [← le_llp_iff_le_rlp]
     exact transfiniteCompositions_le_llp_rlp W
 
+instance [(monomorphisms C).IsStableUnderCobaseChange] [HasPushouts C]
+    [HasPullbacks C] (J : Type*) [Category J] :
+    (monomorphisms (J ⥤ C)).IsStableUnderCobaseChange := by
+  rw [← functorCategory_monomorphisms]
+  infer_instance
+
 end CategoryTheory.MorphismProperty
 
 namespace SSet
 
-instance : IsStableUnderTransfiniteComposition.{u} (monomorphisms (SSet.{u})) := sorry
-instance : IsStableUnderCobaseChange (monomorphisms (SSet.{u})) := sorry
+instance : IsStableUnderTransfiniteComposition.{u} (monomorphisms (SSet.{u})) where
+  isStableUnderTransfiniteCompositionOfShape J _ _ _ _ := by
+    change (monomorphisms (_ ⥤ _)).IsStableUnderTransfiniteCompositionOfShape _
+    infer_instance
+
+instance : IsStableUnderCobaseChange (monomorphisms (SSet.{u})) :=
+  inferInstanceAs (monomorphisms (_ ⥤ _)).IsStableUnderCobaseChange
+
 instance : IsStableUnderCoproducts.{u} (monomorphisms (SSet.{u})) := sorry
 
 namespace modelCategoryQuillen
