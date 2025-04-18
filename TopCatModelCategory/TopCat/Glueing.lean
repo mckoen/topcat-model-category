985662e8ab3951fa5163d43cc76308f52f5566ce
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-04-19 00:41:40 +0200

    wip

--- TopCatModelCategory/TopCat/Glueing.lean
+++ TopCatModelCategory/TopCat/Glueing.lean
@@ -31,12 +31,16 @@ namespace TopCat
 variable {X : TopCat.{u}} {ι : Type w} (A : Set X) (U : ι → Set X) (V : ι → ι → Set X)
 
 structure MulticoequalizerDiagram extends CompleteLattice.MulticoequalizerDiagram A U V where
-  top_eq : (TopCat.of A).str = ⨆ (i : ι),
+  eq_iSup_coinduced : (TopCat.of A).str = ⨆ (i : ι),
     (TopCat.of (U i)).str.coinduced (fun ⟨x, hx⟩ ↦ ⟨x, toMulticoequalizerDiagram.le i hx⟩)
 
 namespace MulticoequalizerDiagram
 
-variable {A U V} (h : MulticoequalizerDiagram A U V)
+variable {A U V}
+
+section
+
+variable (h : MulticoequalizerDiagram A U V)
 
 nonrec abbrev multispanIndex : MultispanIndex (.prod ι) TopCat.{u} :=
   h.multispanIndex.map Set.functorToTopCat
@@ -46,11 +50,10 @@ nonrec abbrev multicofork : Multicofork h.multispanIndex :=
 
 lemma continuous_iff {B : Type*} [TopologicalSpace B] (f : A → B) :
     Continuous f ↔ ∀ (i : ι), Continuous (fun (⟨x, hx⟩ : U i) ↦ f ⟨x, h.le i hx⟩) := by
-  trans Continuous[(TopCat.of A).str, _] f
-  · rfl
-  · rw [h.top_eq, continuous_iSup_dom]
-    simp only [continuous_coinduced_dom]
-    rfl
+  change Continuous[(TopCat.of A).str, _] f ↔ _
+  rw [h.eq_iSup_coinduced, continuous_iSup_dom]
+  simp only [continuous_coinduced_dom]
+  rfl
 
 noncomputable def multicoforkIsColimit : IsColimit h.multicofork :=
   Multicofork.IsColimit.mk _
@@ -71,6 +74,53 @@ noncomputable def multicoforkIsColimit : IsColimit h.multicofork :=
           Eq.trans (by dsimp; rw [← hm]; rfl)
             ((Types.isColimitMulticoforkMapSetToTypes h.toMulticoequalizerDiagram).fac
               (Multicofork.map s (forget _)) (.right j)).symm)))
+
+end
+
+lemma mk_of_isOpen (h : CompleteLattice.MulticoequalizerDiagram A U V)
+    (hU : ∀ i, IsOpen (U i)) :
+    TopCat.MulticoequalizerDiagram A U V where
+  toMulticoequalizerDiagram := h
+  eq_iSup_coinduced := by
+    ext W
+    simp only [isOpen_iSup_iff, isOpen_coinduced]
+    let f (i : ι) : U i → A := fun ⟨x, hx⟩ ↦ ⟨x, h.le i hx⟩
+    have hf (i : ι) : Continuous (f i) := by continuity
+    refine ⟨fun hF i ↦ IsOpen.preimage (hf i) hF, fun hF ↦ ?_⟩
+    have hf' (i : ι) : IsOpenEmbedding (f i) :=
+      { toIsEmbedding := IsEmbedding.inclusion (h.le i)
+        isOpen_range := by
+          rw [isOpen_induced_iff]
+          aesop }
+    have : W = ⋃ (i : ι), (f i) '' ((f i) ⁻¹' W) := by
+      ext ⟨x, hx⟩
+      rw [← h.iSup_eq] at hx
+      aesop
+    rw [this]
+    exact isOpen_iUnion (fun i ↦ (hf' i).isOpenMap _ (hF i))
+
+lemma mk_of_isClosed [Finite ι] (h : CompleteLattice.MulticoequalizerDiagram A U V)
+    (hU : ∀ i, IsClosed (U i)) :
+    TopCat.MulticoequalizerDiagram A U V where
+  toMulticoequalizerDiagram := h
+  eq_iSup_coinduced := by
+    rw [TopologicalSpace.ext_iff_isClosed]
+    intro F
+    simp only [isClosed_iSup_iff, isClosed_coinduced]
+    let f (i : ι) : U i → A := fun ⟨x, hx⟩ ↦ ⟨x, h.le i hx⟩
+    have hf (i : ι) : Continuous (f i) := by continuity
+    refine ⟨fun hF i ↦ IsClosed.preimage (hf i) hF, fun hF ↦ ?_⟩
+    have hf' (i : ι) : IsClosedEmbedding (f i) :=
+      { toIsEmbedding := IsEmbedding.inclusion (h.le i)
+        isClosed_range := by
+          rw [isClosed_induced_iff]
+          aesop }
+    have : F = ⋃ (i : ι), (f i) '' ((f i) ⁻¹' F) := by
+      ext ⟨x, hx⟩
+      rw [← h.iSup_eq] at hx
+      aesop
+    rw [this]
+    exact isClosed_iUnion_of_finite (fun i ↦ (hf' i).isClosedMap _ (hF i))
 
 end MulticoequalizerDiagram
 
